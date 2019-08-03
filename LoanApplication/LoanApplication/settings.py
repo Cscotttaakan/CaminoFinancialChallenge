db83cd66726bf9c645526369142c5dba746a23bc
Craig Scott <cscott_taakan@yahoo.com>
2019-08-03 01:56:10 -0700

    deployment

--- LoanApplication/LoanApplication/settings.py
+++ LoanApplication/LoanApplication/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://stark-refuge-31370.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
