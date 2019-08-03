3e0ebda6f575a6ab7cb755fe927eed484eed0785
Craig Scott <cscott_taakan@yahoo.com>
2019-08-03 00:57:03 -0700

    Finished Tests

--- LoanApplication/LoanApplication/settings.py
+++ LoanApplication/LoanApplication/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','testserver']
 
 # Application definition
 
--- LoanApplication/loans/tests.py
+++ LoanApplication/loans/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
 from rest_framework import status
@@ -17,17 +18,46 @@ import os
 
 class CreateApplicationViewTestCase(APITestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
     def tearDown(self):
         Application.objects.all().delete()
+        User.objects.all().delete()
 
     def testCreate(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         with open(os.path.join(dir_path,'sample.json'), 'r') as json_file:
-            print(json_file)
             request_params = json.load(json_file)
-            create_url = reverse('loans:createapp')
-            resp = self.client.post(
-                    url=create_url,
-                    data=request_params,
-                    )
-            self.assertTrue(resp.status_code==200)
+        create_url = reverse('loans:createapp')
+        resp = self.client.post(create_url,json.dumps(request_params),content_type='application/json')
+        print(resp.status_code)
+        self.assertTrue(resp.status_code==200)
+
+class StatusApplicationViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='password')
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def tearDown(self):
+        Application.objects.all().delete()
+        User.objects.all().delete()
+
+    def testCreate(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(dir_path,'sample.json'), 'r') as json_file:
+            request_params = json.load(json_file)
+        create_url = reverse('loans:createapp')
+        resp = self.client.post(create_url,json.dumps(request_params),content_type='application/json')
+        appid = resp.data['AppID']
+        self.assertTrue(resp.status_code==200)
+
+        create_url = reverse('loans:status')
+        resp = self.client.get(create_url,data={'id' : appid})
+        self.assertTrue(resp.data['Status'] == 'Pending')
+
+
